df7c928a9af51d75821ded5b8e545859d37bbd30
1seyoung <akdlrnal0705@gmail.com>
2025-07-08 15:31:32 +0900

    init: initial commit

---
+++ src/main/java/com/unear/pos/UnearPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unear.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnearPosBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnearPosBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/unear/pos/UnearPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unear.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnearPosBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
