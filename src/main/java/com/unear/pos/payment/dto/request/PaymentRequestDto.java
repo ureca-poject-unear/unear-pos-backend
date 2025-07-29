2c4b07010b870e95f88e3de7d89a85b5e7535a3b
1seyoung <akdlrnal0705@gmail.com>
2025-07-29 14:21:41 +0900

    faet : 생성자 추가

--- src/main/java/com/unear/pos/payment/dto/request/PaymentRequestDto.java
+++ src/main/java/com/unear/pos/payment/dto/request/PaymentRequestDto.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequestDto {
     @NotNull(message = "결제 금액은 필수입니다")
     @Min(value = 1, message = "결제 금액은 1원 이상이어야 합니다")
