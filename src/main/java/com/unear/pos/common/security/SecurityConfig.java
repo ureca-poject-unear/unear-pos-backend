6cdc7958c788bfd83da634f3e7f6cd6e377931cd
1seyoung <akdlrnal0705@gmail.com>
2025-07-30 05:18:06 +0900

    chore : oring 설정

--- src/main/java/com/unear/pos/common/security/SecurityConfig.java
+++ src/main/java/com/unear/pos/common/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     private static final String[] WHITELIST = {
             "/auth/login",
             "/auth/register",
+            "/membership/verify"
     };
 
     @Bean
