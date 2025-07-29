9e11fb3ef46e36e9863781cd27a66813ab85782a
1seyoung <akdlrnal0705@gmail.com>
2025-07-29 16:27:31 +0900

    feat : cors 경로 추가

--- src/main/java/com/unear/pos/common/security/SecurityConfig.java
+++ src/main/java/com/unear/pos/common/security/SecurityConfig.java
@@ -60,8 +60,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4000", "https://localhost:3000",
-                "https://localhost:4000"));
+        config.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://localhost:4000", "https://localhost:3000", "https://v0.dev",
+                        "https://localhost:4000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
