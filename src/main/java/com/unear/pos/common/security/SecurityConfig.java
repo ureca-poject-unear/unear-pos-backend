c0349203b44bb7c0361992ad8f01f6ee633e7ec9
1seyoung <akdlrnal0705@gmail.com>
2025-07-30 09:34:33 +0900

    chore : oring 설정

--- src/main/java/com/unear/pos/common/security/SecurityConfig.java
+++ src/main/java/com/unear/pos/common/security/SecurityConfig.java
@@ -23,8 +23,6 @@ public class SecurityConfig {
 
     private static final String[] WHITELIST = {
             "/auth/login",
-            "/auth/register",
-            "/membership/verify"
     };
 
     @Bean
@@ -65,7 +63,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
-                List.of("http://127.0.0.1:5500", "http://localhost:5500")); // 또는 "http://dev.unear.site"
+                List.of("http://127.0.0.1:5500", "http://localhost:5500",
+                        "http://localhost:4000")); // 또는 "http://dev.unear.site"
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
