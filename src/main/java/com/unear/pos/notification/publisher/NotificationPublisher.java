916ffc945319aad97f1c680bb92f88a2b8785915
1seyoung <akdlrnal0705@gmail.com>
2025-08-05 17:52:44 +0900

    feat : Redis Stream 으로 버전 변경

--- src/main/java/com/unear/pos/notification/publisher/NotificationPublisher.java
+++ src/main/java/com/unear/pos/notification/publisher/NotificationPublisher.java
@@ -3,8 +3,12 @@ package com.unear.pos.notification.publisher;
 import com.unear.pos.notification.dto.PosNotificationEventRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StreamOperations;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
@@ -12,14 +16,22 @@ import org.springframework.stereotype.Component;
 public class NotificationPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private static final String NOTIFICATION_CHANNEL = "pos_notification_channel";
+    private static final String NOTIFICATION_STREAM = "pos_notification_stream";
 
     public void publishNotification(PosNotificationEventRequest request) {
         try {
-            redisTemplate.convertAndSend(NOTIFICATION_CHANNEL, request);
-            log.info("알림 발행 성공 - type: {}, userId: {}", request.getType(), request.getUserId());
+            StreamOperations<String, Object, Object> streamOps = redisTemplate.opsForStream();
+
+            ObjectRecord<String, PosNotificationEventRequest> record = StreamRecords
+                    .newRecord()
+                    .in(NOTIFICATION_STREAM)
+                    .ofObject(request);
+
+            streamOps.add(record);
+
+            log.info("알림 발행 성공 (Redis Stream) - type: {}, userId: {}", request.getType(), request.getUserId());
         } catch (Exception e) {
-            log.error("알림 발행 실패 - type: {}, userId: {}, reason: {}",
+            log.error("알림 발행 실패 (Redis Stream) - type: {}, userId: {}, reason: {}",
                     request.getType(), request.getUserId(), e.getMessage(), e);
         }
     }
